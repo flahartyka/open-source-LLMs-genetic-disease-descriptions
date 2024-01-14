5bd880c13c51469e70ce13a134ceacfb6a09d8ef
flahartyka <156475178+flahartyka@users.noreply.github.com>
2024-01-14 09:16:33 -0500

    Create vicuna_13.sh

---
+++ vicuna/vicuna_13.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /data/$USER/conda/etc/profile.d/conda.sh && source /data/$USER/conda/etc/profile.d/mamba.sh
+
+mamba activate base
+
+cd /data/$USER/llm_project/FastChat/fastchat/llm_judge
+
+export HF_HOME=/data/$USER/llm_project/FastChatPretrainedModels
+export HF_DATASETS_CACHE=/data/$USER/llm_project/FastChatPretrainedModels
+
+python3 gen_model_answer.py --model-path lmsys/vicuna-13b-v1.3 --model-id vicuna-13b-v1.3 --answer-file "data/mt_bench/model_answer/vicuna-13/q1_result.jsonl" --question-file "data/mt_bench/vicuna_q1.jsonl" 
+
+# The 63 questions are divided into 8 files, to reduce runtime
+# questions are located in 'llm_project/FastChat/fastchat/llm_judge/data/mt_bench'
+# answers are located in 'llm_project/FastChat/fastchat/llm_judge/data/mt_bench/model_answer/vicuna-13'
