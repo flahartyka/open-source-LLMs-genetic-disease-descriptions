75952a2490a0c78b915fcac408dcb5b416f48abe
flahartyka <156475178+flahartyka@users.noreply.github.com>
2024-01-14 09:17:00 -0500

    Update vicuna_33.sh

--- vicuna/vicuna_33.sh
+++ vicuna/vicuna_33.sh
@@ -9,6 +9,8 @@ cd /data/$USER/llm_project/FastChat/fastchat/llm_judge
 export HF_HOME=/data/$USER/llm_project/FastChatPretrainedModels
 export HF_DATASETS_CACHE=/data/$USER/llm_project/FastChatPretrainedModels
 
-python3 gen_model_answer.py --model-path lmsys/vicuna-33b-v1.3 --model-id vicuna-33b-v1.3 --answer-file "data/mt_bench/model_answer/vicuna-33/q1_rare_ans.jsonl" --question-file "data/mt_bench/q1_rare.jsonl" 
+python3 gen_model_answer.py --model-path lmsys/vicuna-33b-v1.3 --model-id vicuna-33b-v1.3 --answer-file "data/mt_bench/model_answer/vicuna-33/q1_result.jsonl" --question-file "data/mt_bench/vicuna_q1.jsonl" 
 
 # The 63 questions are divided into 8 files, to reduce runtime
+# questions are located in 'llm_project/FastChat/fastchat/llm_judge/data/mt_bench'
+# answers are located in 'llm_project/FastChat/fastchat/llm_judge/data/mt_bench/model_answer/vicuna-33'
